03baaa8685fa212bf442128d48692519dda210cd
jbaldwin87 <jbaldwin@monusb.ca>
2017-10-18 11:35:10 -0500

    ex 6

--- mobile_4/sketch.js
+++ mobile_4/sketch.js
@@ -1,27 +1,27 @@
-var obstacles = [];
+var x = 0;
+var y = 0;
+
+var Vx = 0;
+var Vy = 0;
+
+var Xx = 0;
+var Xy = 0;
+
+var Multix = 0;
+var Multiy = 0;
 
 function setup() {
- createCanvas(windowWidth, windowHeight);
- for (var i=0; i < 1000; i++) {
-  obstacles[i] = new Obstacle("malus");
- }
+ createCanvas(windowWidth,windowHeight); 
 }
 
 function draw() {
- for (var i=0; i < 1000; i++) {
-  obstacles[i].display();
- }
+ background(255);
+ textSize(40);
+ text("Rx: " + rotationX, 100, 100);
+ text("Ry: " + rotationY, 100, 150);
+ text("Rz: " + rotationZ, 100, 200);
+
 }
-
-function Obstacle(genre) {
- this.type = genre;
- this.x = random(0, windowWidth);
- this.y = random(0, windowHeight);
- this.size = random(20, 50);
- this.couleur = color(random(0, 255), random(0, 255), random(0, 255));
-
- this.display = function() {
-  fill(this.couleur);
-  ellipse(this.x, this.y, this.size, this.size);
-  }
+function touchStarted() {
+ background(random(0,255),random(0,255),random(0,255));
 }
